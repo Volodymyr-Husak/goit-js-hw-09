f55414ef6e71b501b20fb6b43ec5852d39eff87b
Volodymyr Husak <volodyaog2907@gmail.com>
2022-08-28 22:54:17 +0300

    creat task 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,7 +2,27 @@ function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
     // Reject
+    reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    // console.log(`❌ Rejected promise ${position} in ${delay}ms`)
   }
+}
+// const btnSubmitEl = document.querySelector("button[type='submit']");
+const form = document.querySelector('.form');
+// console.log(btnSubmitEl);
+// formEl.addEventListener('submit', onFormSubmit);
+form.addEventListener('submit', onSubmit);
+// console.log('Hi');
+function onSubmit(event) {
+  // preventDefault();
+  event.preventDefault();
+  // console.log('Hi');
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+  // event.target;
+  console.log(delay.value);
 }
