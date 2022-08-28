e202c9b1515fbe44066d2ee7ffb1eb1090275148
Volodymyr Husak <volodyaog2907@gmail.com>
2022-08-28 21:21:20 +0300

    corrects task 02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,6 +3,8 @@ import flatpickr from 'flatpickr';
 // Додатковий імпорт стилів
 import 'flatpickr/dist/flatpickr.min.css';
 // console.log(selectedDates[0]);
+import Notiflix from 'notiflix';
+
 let dateSelected = 0;
 let dateNow = 0;
 
@@ -12,63 +14,48 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    // console.log(selectedDates[0]);
-    // const dateSelected = new Date(selectedDates[0]).getTime();
     dateSelected = new Date(selectedDates[0]).getTime();
     dateNow = new Date().getTime();
 
     if (dateSelected < dateNow) {
-      window.alert('Please choose a date in the future');
+      // window.alert('Please choose a date in the future');
+      Notiflix.Notify.failure('Please choose a date in the future');
     } else btnStartEl.disabled = false;
   },
 };
-// const date = new Date();
-// console.log(date.getTime());
+
 const btnStartEl = document.querySelector('button[data-start');
 const inputEl = document.querySelector('input[type="text"]');
 const valueElArr = document.querySelectorAll('.value');
-// console.log(valueElArr);
 
-const fp = flatpickr(inputEl, options); // flatpickr
+const fp = flatpickr(inputEl, options);
 
 btnStartEl.disabled = true;
 
 btnStartEl.addEventListener('click', onClick);
 
 function onClick() {
-  //   dateNow = new Date().getTime();
-  //   const dateDifference = dateSelected - dateNow;
-  //   //   console.log(dateDifference);
-  //   //   console.log(convertMs(dateDifference));
-  //   const dateObj = convertMs(dateDifference);
-  //   const { days, hours, minutes, seconds } = dateObj;
-  //   valueElArr[0].textContent = days;
-  //   valueElArr[1].textContent = hours;
-  //   valueElArr[2].textContent = minutes;
-  //   valueElArr[3].textContent = seconds;
+  setInterval(addsValueInterface, 1000);
+}
 
-  setInterval(() => {
-    dateNow = new Date().getTime();
-    const dateDifference = dateSelected - dateNow;
-    if (dateSelected < dateNow) {
-      return;
-    }
-    //   console.log(dateDifference);
-    //   console.log(convertMs(dateDifference));
-    const dateObj = convertMs(dateDifference);
-    const { days, hours, minutes, seconds } = dateObj;
-    valueElArr[0].textContent = days;
-    valueElArr[1].textContent = hours;
-    valueElArr[2].textContent = minutes;
-    valueElArr[3].textContent = seconds;
-  }, 1000);
+function addsValueInterface() {
+  dateNow = new Date().getTime();
+  const dateDifference = dateSelected - dateNow;
+  if (dateSelected < dateNow) {
+    return;
+  }
+
+  const dateObj = convertMs(dateDifference);
+  const { days, hours, minutes, seconds } = dateObj;
+  valueElArr[0].textContent = days;
+  valueElArr[1].textContent = hours;
+  valueElArr[2].textContent = minutes;
+  valueElArr[3].textContent = seconds;
 }
 
 function addLeadingZero(value) {
-  //   return Number(String(value).padStart(2, '0'));
   return String(value).padStart(2, '0');
 }
-// console.log(addLeadingZero(2));
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -80,25 +67,27 @@ function convertMs(ms) {
   // Remaining days
   const days = addLeadingZero(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  addLeadingZero();
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
 
   return { days, hours, minutes, seconds };
 }
 // ==========================================================
 const timerEl = document.querySelector('.timer');
-// console.log(timerEl);
+
 timerEl.style.display = 'flex';
 timerEl.style.justifyContent = 'center';
 const fieldsEl = document.querySelectorAll('.field');
-// console.log(fieldsEl);
+
 fieldsEl.forEach(field => {
-  //   field.style.display = 'block';
-  field.style.outline = 'solid';
-  field.style.backgroundColor = 'tomato';
+  // field.style.outline = 'solid';
+  field.style.backgroundColor = '#0cb7eb';
   field.style.margin = '20px';
   field.style.padding = '5px';
   field.style.borderRadius = '5px';
