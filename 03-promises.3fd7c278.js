62e627b1e1a57f8c398b1b47891e1ca912222d59
Volodymyr-Husak <102227262+Volodymyr-Husak@users.noreply.github.com>
2022-08-28 19:54:39 +0000

    Deploying to gh-pages from @ Volodymyr-Husak/goit-js-hw-09@f55414ef6e71b501b20fb6b43ec5852d39eff87b 🚀

---
+++ 03-promises.3fd7c278.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault();var t=e.currentTarget.elements,n=t.delay;t.step,t.amount,console.log(n.value)}));
+//# sourceMappingURL=03-promises.3fd7c278.js.map
---
+++ 03-promises.cd0b4338.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault();const{elements:{delay:t,step:n,amount:o}}=e.currentTarget;console.log(t.value)}));
+//# sourceMappingURL=03-promises.cd0b4338.js.map
