f9243d2135514665a352d20e310dd60c93359e68
Volodymyr Husak <volodyaog2907@gmail.com>
2022-08-29 16:51:35 +0300

    corrects task 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,28 +1,51 @@
+import Notiflix from 'notiflix';
+
+let positionProm = 0;
+let delayValue = 0;
+let stepValue = 0;
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-    resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  } else {
-    // Reject
-    reject(`❌ Rejected promise ${position} in ${delay}ms`);
-    // console.log(`❌ Rejected promise ${position} in ${delay}ms`)
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+  return promise;
 }
-// const btnSubmitEl = document.querySelector("button[type='submit']");
+
 const form = document.querySelector('.form');
-// console.log(btnSubmitEl);
-// formEl.addEventListener('submit', onFormSubmit);
+
 form.addEventListener('submit', onSubmit);
-// console.log('Hi');
+
 function onSubmit(event) {
-  // preventDefault();
   event.preventDefault();
-  // console.log('Hi');
+
   const {
     elements: { delay, step, amount },
   } = event.currentTarget;
-  // event.target;
-  console.log(delay.value);
+  positionProm = 0;
+
+  delayValue = Number(delay.value);
+  stepValue = Number(step.value);
+  setInterval(() => {
+    positionProm += 1;
+    if (positionProm > amount.value) {
+      return;
+    }
+
+    createPromise(positionProm, delayValue)
+      .then(resolve => {
+        Notiflix.Notify.success(resolve);
+      })
+      .catch(reject => {
+        Notiflix.Notify.failure(reject);
+      });
+
+    delayValue += stepValue;
+  }, stepValue);
 }
